c25efb52d79b2308fd774128f9ef3f03cbf8ddd4
Cezara24 <cezarateodorescu8@gmail.com>
2022-12-05 21:24:40 +0200

    fragments, portals and refs

--- 08 Time to Practice_ A Complete Practice Project/src/components/Users/AddUser.js
+++ 08 Time to Practice_ A Complete Practice Project/src/components/Users/AddUser.js
@@ -1,23 +1,81 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import Card from "../UI/Card";
-import styles from "./AddUser.module.css";
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).'
+      });
+      return;
+    }
+    if (+enteredAge < 0) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).'
+      });
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
+  const usernameChangeHandler = (event) => {
+    if(event.target.value.includes(" ")){	
+      event.target.value = event.target.value.replace(/ /g,'');	
+    }
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+
+    event.target.value = event.target.value.replace(/[e\+\-]/gi,'');
+    if(event.target.value[0] === "0" && event.target.value.length > 1){
+      event.target.value = event.target.value.slice(1);
+    }
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
-      </form>
-    </Card>
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            min="0"
+            max="130"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
